a222b91e5a69247480fa1999c7a7de1a003d38b5
ayodeji akinola <ayodejiakinola007@gmail.com>
2024-11-26 07:26:48 +0100

    adadw

--- server/dist/src/server.js
+++ server/dist/src/server.js
@@ -14,7 +14,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
-const body_parser_1 = __importDefault(require("body-parser"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const cors_1 = __importDefault(require("cors"));
 const userRoute_1 = __importDefault(require("../routes/userRoute"));
@@ -26,7 +25,6 @@ dotenv_1.default.config();
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 3000;
 // Middleware
-app.use(body_parser_1.default.json({ limit: '10mb' }));
 app.use(express_1.default.urlencoded({ extended: true, limit: '10mb' }));
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
